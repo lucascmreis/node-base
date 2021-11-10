8d42873456ccd98227f37544b912b50c4c86c015
lucas.creis <lucas.creis@b2wdigital.com>
2021-11-09 21:20:25 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
