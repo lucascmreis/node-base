f06da20a6d673332c71d3a54c893c60f274e8b69
lucas.creis <lucas.creis@b2wdigital.com>
2021-11-11 00:54:00 -0300

    refactor: costumized error missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup'
+import { MissingParamError } from '../erros/missing-param-error'
 
 describe('SignUp Controller', () => {
   it('Should return 400 if no name is provided ', () => {
@@ -14,7 +15,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   it('Should return 400 if no email is provided ', () => {
@@ -29,6 +30,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
