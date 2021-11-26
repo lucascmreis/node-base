0d031d7159a5cd19c482915caa8c6bb58501390b
lucas.creis <lucas.creis@b2wdigital.com>
2021-11-26 07:33:32 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/domain/useCases/add-account.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from '../models/account'
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  // eslint-disable-next-line @typescript-eslint/method-signature-style
+  add(account: AddAccountModel): AccountModel
+}
