b46b1190c02bc1ebbd26deccfba183b0ffeef10c
lucas.creis <lucas.creis@b2wdigital.com>
2021-12-10 08:09:35 -0300

    feat: create a mapping method in MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,9 +7,8 @@ export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const { insertedId } = await accountCollection.insertOne(accountData)
-    const { _id, ...accountWithoutId } = await accountCollection.findOne(insertedId)
-    const accountParsed = Object.assign({}, accountWithoutId, { id: _id.toHexString() }) as AccountModel
-    console.log(accountParsed)
-    return accountParsed
+    const account = await accountCollection.findOne(insertedId)
+
+    return MongoHelper.map(account)
   }
 }
